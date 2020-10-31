18ae8faeee075b4809fe48430934f872aadb494b
RandallConrad <BrandallConrad@gmail.com>
2020-10-30 17:14:47 -0700

    Added onClick transitions and navBar functionality

---
+++ scrollTo.js
@@ -0,0 +1,33 @@
+$(document).ready(function ()
+{
+    $("#logo").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#pageTop").offset().top
+        }, 1000);
+    });
+
+    $("#navExperience").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#experience").offset().top
+        }, 1000);
+    });
+
+    $("#navSkills").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 1000);
+    });
+
+    $("#navProject").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000);
+    });
+
+    $("#navContact").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+});
